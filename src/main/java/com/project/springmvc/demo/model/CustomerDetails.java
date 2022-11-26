2cc7d217bdb765c0588c43a54553794c149d07e4
Manikandan Balakrishnan <yamimano@Manikandans-MacBook-Air.local>
2022-11-26 12:06:26 -0500

    Removed unwanted Code

--- src/main/java/com/project/springmvc/demo/model/CustomerDetails.java
+++ src/main/java/com/project/springmvc/demo/model/CustomerDetails.java
@@ -2,10 +2,7 @@ package com.project.springmvc.demo.model;
 
 import java.io.Serializable;
 
-import lombok.Getter;
-import lombok.Setter;
 
-@Getter @Setter
 public class CustomerDetails implements Serializable {
 
 	private static final long serialVersionUID = 1L;
--- src/main/java/com/project/springmvc/demo/repository/PersonsRepository.java
+++
@@ -1,9 +0,0 @@
-package com.project.springmvc.demo.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.project.springmvc.demo.entity.Persons;
-
-public interface PersonsRepository extends JpaRepository<Persons, Integer>{
-
-}
